238d7f3a716f06f69cc895c399f252520091e2b3
Simon Lally <simonlallydev@gmail.com>
2022-08-26 21:50:36 -0400

    yeet

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2022_08_26_221911) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "timescaledb"
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
